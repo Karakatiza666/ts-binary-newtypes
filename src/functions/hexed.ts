f8439b87f7edd427df6514eccb9d774d025c46ce
George <bulakh.96@gmail.com>
2023-03-17 17:34:52 +0200

    Add unsafeFromHexed

--- src/functions/hexed.ts
+++ src/functions/hexed.ts
@@ -1,4 +1,4 @@
-import { fromHex, toHex } from "src/functions/hex"
+import { fromHex, makeHex, toHex } from "src/functions/hex"
 import type { Hex } from "src/functions/hex"
 
 // key [Hexed] makes type unique from Hex, type T makes Hexeds of different types unique
@@ -10,6 +10,8 @@ export const mapFromHexed = <T>(c: { from_bytes: (bytes: Uint8Array) => T }) => 
    hexeds.map(fromHexed(c))
 export const fromHexed = <T>(c: { from_bytes: (bytes: Uint8Array) => T }) => (hexed: Hexed<T>) =>
    c.from_bytes(fromHex(hexed))
+export const unsafeFromHexed = <T>(c: { from_bytes: (bytes: Uint8Array) => T }) => (hexed: string) =>
+   c.from_bytes(fromHex(makeHex(hexed)))
 
 export const toHexed = <T extends { to_bytes: () => Uint8Array }>(v: T) => unsafeToHexed<T>(v.to_bytes())
 export const unsafeToHexed = <T>(b: Uint8Array) => toHex(b) as Hexed<T>
