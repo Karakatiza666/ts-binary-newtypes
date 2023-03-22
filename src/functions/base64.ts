6fb8563d452bcf73f9052a7ab5beb6ab50cbf646
George <bulakh.96@gmail.com>
2023-03-22 15:52:27 +0200

    Add type guard for isBase64

--- src/functions/base64.ts
+++ src/functions/base64.ts
@@ -7,7 +7,7 @@ declare const Base64: unique symbol
 // https://stackoverflow.com/questions/475074/regex-to-parse-or-validate-base64-data
 const base64regex = /^[+/0-9A-Za-z]+={0,2}$/
 
-export const isBase64 = (str: string) => str.length % 4 === 0 && base64regex.test(str)
+export const isBase64 = (str: string): str is Base64 => str.length % 4 === 0 && base64regex.test(str)
 export function assertBase64(str: string): asserts str is Base64 {
    if (!isBase64(str)) {
       throw new Error('Not a base64 string!')
